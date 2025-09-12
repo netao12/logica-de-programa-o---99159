37ea1aa52b71049e9f00c2c1eff15f111a121792
netao12 <aleixojosent@gmail.com>
2025-09-12 16:55:22 -0300

    Add files via upload

---
+++ while_4.py
@@ -0,0 +1,17 @@
+import os 
+os.system("Cls") 
+
+QUANTIDADE_NOTAS = 2 
+soma = 0
+ 
+for i in range(QUANTIDADE_NOTAS):
+    while True:
+        nota = int(input(f"Digite a {i+1}ª nota: ")) 
+        if nota >= 0 and nota <= 10:
+            break
+        soma = soma + nota
+
+
+media = soma / QUANTIDADE_NOTAS
+
+print(f"Média: {media}")
